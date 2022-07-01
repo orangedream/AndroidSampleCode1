5fad53995d2dfe1ef10fa9323d87ebe753b4bc74
Orange Chang <orange@iotec.tw>
2022-07-02 04:42:18 +0800

    Activity transition example 1. Basic syntax 2. Slide in/out syntax 3. Switch new root activity

--- app/src/main/java/tw/iotec/androidsamplecode1/MainActivity.kt
+++ app/src/main/java/tw/iotec/androidsamplecode1/MainActivity.kt
@@ -1,10 +1,10 @@
 package tw.iotec.androidsamplecode1
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,22 +21,28 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btn_next_activity_v2).setOnClickListener {
             startActivity(
                 Intent(
-                    baseContext,
+                    this,
                     SecondActivity::class.java
                 )
             )
             overridePendingTransition(
                 // 指定轉換的動畫
-                R.anim.slide_in_right,   // 新 Activity 進入動畫
-                R.anim.slide_out_left	 // 原 Activity 離開的動畫
+                R.anim.slide_in_right,	 // 新 Activity 進入動畫
+                R.anim.slide_out_left   // 原 Activity 離開的動畫
             )
         }
 
-        findViewById<Button>(R.id.btn_next_activity).setOnClickListener {
+        findViewById<Button>(R.id.btn_next_activity_v3).setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java).apply {
-                putExtra(AlarmClock.EXTRA_MESSAGE, "Hello, second.") // pass value to the activity
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             }
             startActivity(intent)
+            finish()
+            overridePendingTransition(
+                // 指定轉換的動畫
+                R.anim.slide_in_right,	 // 新 Activity 進入動畫
+                R.anim.slide_out_left   // 原 Activity 離開的動畫
+            )
         }
 
     }
--- app/src/main/java/tw/iotec/androidsamplecode1/SecondActivity.kt
+++ app/src/main/java/tw/iotec/androidsamplecode1/SecondActivity.kt
@@ -1,5 +1,6 @@
 package tw.iotec.androidsamplecode1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
@@ -17,6 +18,31 @@ class SecondActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_main_activity).setOnClickListener {
             finish()
+        }
+
+
+        findViewById<Button>(R.id.btn_main_activity_v2).setOnClickListener {
+            finish()
+            overridePendingTransition(
+                // 指定轉換的動畫
+                R.anim.slide_in_left,   // 新 Activity 進入動畫
+                R.anim.slide_out_right	 // 原 Activity 離開的動畫
+            )
+
+        }
+
+        findViewById<Button>(R.id.btn_main_activity_v3).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            startActivity(intent)
+            finish()
+            overridePendingTransition(
+                // 指定轉換的動畫
+                R.anim.slide_in_left,   // 新 Activity 進入動畫
+                R.anim.slide_out_right	 // 原 Activity 離開的動畫
+            )
+
         }
 
     }
